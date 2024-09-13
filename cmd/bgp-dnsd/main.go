b91675ae0b06d7fea2a84bd87bf025da1d225a18
Leonid Korokh <lkorokh@gmail.com>
2024-09-13 06:54:23 +0300

    WIP

--- cmd/bgp-dnsd/main.go
+++ cmd/bgp-dnsd/main.go
@@ -1,5 +1,17 @@
 package main
-
+import (
+    "github.com/red55/bgp-dns/internal/config"
+    "github.com/spf13/pflag"
+)
 func main() {
+    pflag.StringP("config", "c", "appsettings.yml", "Path to configuration file.")
+    pflag.Parse()
 
+    fn := pflag.Lookup("config")
+    configPath, e := filepath.Abs(fn.Value.String())
+    if e != nil {
+        log.L().Fatalf("Wrong path to configuration file")
+    }
+
+    config.Init(configPath)
 }
