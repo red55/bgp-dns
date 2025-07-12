0a812e747b38d15016659c0d83b4346b060d3075
Leonid Korokh <lkorokh@gmail.com>
2025-07-12 12:18:55 +0300

    style: format code for improved readability in loop.go

--- internal/dns/loop.go
+++ internal/dns/loop.go
@@ -9,13 +9,12 @@ import (
 	"time"
 )
 
-
 func (c *cache) loop(ctx context.Context) {
 	c.wg.Add(1)
 	defer c.wg.Done()
 
 	cfg := ctx.Value("cfg").(*config.AppCfg)
-	L:
+L:
 	for {
 		var sleepUntil time.Time
 		now := time.Now()
@@ -28,7 +27,7 @@ func (c *cache) loop(ctx context.Context) {
 			sleepUntil = time.Now().Add(cfg.Dns.Cache.MinTtl * time.Second)
 		}
 
-		for k,v := range all {
+		for k, v := range all {
 			ce := v.(*cacheEntry)
 
 			c.L().Trace().Msgf("%s, ttl:%d, expire: %s", k.(string), ce.ttl, ce.expiration.Format(time.RFC3339))
@@ -39,31 +38,34 @@ func (c *cache) loop(ctx context.Context) {
 				c.L().Debug().Msgf("Resolving cached %s", k.(string))
 				// resolve will call cache.upsert on resolved IPs
 				c.resolve(nil, q, false)
-				c.L().Trace().Msgf("New %s, ttl:%d, expire: %s", k.(string), ce.ttl,ce.expiration.Format(time.RFC3339))
+				c.L().Trace().Msgf("Resolved cached %s, ttl:%d, expire: %s", k.(string), ce.ttl, ce.expiration.Format(time.RFC3339))
 			}
 
 			if sleepUntil.After(ce.expiration) {
 				sleepUntil = ce.expiration
+				c.L().Trace().Msgf("Sleep until %s for %d seconds", sleepUntil.Format(time.RFC3339),
+					sleepUntil.Sub(now)/time.Second)
 			}
 
 		}
-		if sleepUntil.Sub(now) < cfg.Dns.Cache.MinTtl * time.Second {
+		c.L().Trace().Msgf("Calculated sleep until and now difference in sec %d ", sleepUntil.Sub(now)/time.Second)
+		if sleepUntil.Sub(now) < cfg.Dns.Cache.MinTtl*time.Second {
 			sleepUntil = now.Add(cfg.Dns.Cache.MinTtl * time.Second)
 		}
 
 		c.L().Info().Msgf("DNS Refresher will sleep until %s for %d seconds", sleepUntil.Format(time.RFC3339),
-			sleepUntil.Sub(now) / time.Second)
+			sleepUntil.Sub(now)/time.Second)
 		timeout, cancelTimeout := context.WithDeadline(ctx, sleepUntil)
 
 		select {
-		case o := <- c.ChanOp():
+		case o := <-c.ChanOp():
 			cancelTimeout()
 			c.HandleOp(o)
 			continue
-		case <- timeout.Done():
+		case <-timeout.Done():
 			cancelTimeout()
 			continue
-		case <- ctx.Done():
+		case <-ctx.Done():
 			cancelTimeout()
 			if !errors.Is(ctx.Err(), context.Canceled) {
 				log.L().Error().Err(ctx.Err())
